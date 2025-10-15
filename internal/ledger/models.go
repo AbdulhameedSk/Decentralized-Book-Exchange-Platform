42d6578aaf37abc151df09fe7057222da5724084
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-10-15 19:00:14 +0530

    models

---
+++ internal/ledger/models.go
@@ -0,0 +1,16 @@
+package ledger
+
+type Book struct {
+	ID        string
+	Title     string
+	Author    string
+	OwnerID   string
+	Available bool
+}
+
+type User struct {
+	ID       string
+	Name     string `validate:"required"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+}
