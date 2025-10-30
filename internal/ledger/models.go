25c4c32966c4b5b4e00ceaef57377148151d4139
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-10-30 19:28:49 +0530

    Change in schema

--- internal/ledger/models.go
+++ internal/ledger/models.go
@@ -1,19 +1,18 @@
 package ledger
 
-type Book struct {
-	ID        string `gorm:"primaryKey"`
-	Title     string
-	Author    string
-	OwnerID   string
-	Available bool
-	AddedBy   string
+type User struct {
+    ID       string `gorm:"primaryKey"`
+    Name     string `gorm:"not null"`
+    Email    string `gorm:"unique;not null"`
+    Password string `gorm:"not null"`
+    Books    []Book `gorm:"foreignKey:OwnerID"`
 }
 
-type User struct {
-	ID       string `gorm:"primaryKey"`
-	Name     string
-	Email    string
-	Password string
-	// To export capital B in Books
-	Books []Book `gorm:"foreignKey:OwnerID"`
+type Book struct {
+    ID        string `gorm:"primaryKey"`
+    Title     string `gorm:"not null"`
+    Author    string
+    OwnerID   string
+    Available bool
+    AddedBy   string
 }
